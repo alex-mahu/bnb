3633a0f121ce4fce713c6cfdf428e8ea9f78edb3
Alex Mahu <alexandru.mahu@hotmail.com>
2020-01-26 20:25:37 +0200

    added workaround for svg appearing over site after finding an element and trying to click on it cleaned up pom and added allure-maven plugin in order to easily generate the report added allure.properties for specifying the results location

--- src/main/java/bnb/helpers/DriverHelper.java
+++ src/main/java/bnb/helpers/DriverHelper.java
@@ -1,15 +1,21 @@
 package bnb.helpers;
 
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
 
 import static bnb.constants.ConfigurationConstants.IMPLICIT_WAIT;
+import static java.time.Duration.of;
+import static java.time.temporal.ChronoUnit.MILLIS;
 
 public final class DriverHelper {
 
@@ -18,7 +24,14 @@ public final class DriverHelper {
     public static void waitableClick(WebDriver driver, WebElement element) {
         new WebDriverWait(driver, IMPLICIT_WAIT)
                 .until(ExpectedConditions.elementToBeClickable(element));
-        element.click();
+        try {
+            element.click();
+        } catch (ElementClickInterceptedException ex) {
+            new Actions(driver)
+                    .pause(of(500, MILLIS))
+                    .click(element)
+                    .perform();
+        }
     }
 
     public static void scrollToElement(WebDriver driver, WebElement element) {
